1290ff12f3dd7026ada294e833bc76ff83881d7c
Bob <kohei.suzuki808@gmail.com>
2020-08-01 17:28:26 +0900

    Created another function and declare some objs used for STT

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from flask import Flask, render_template, request
+from lib.DeepSpeech import DeepSpeech
 
 app = Flask(__name__, static_folder='static')
 
+STT_MODEL_PATH = "model/deepspeech-0.7.4-models.pbmm"
+SCORER_MODEL_PATH = "model/deepspeech-0.7.4-models.scorer"
+result_json_path = "results/result.json"
 UPLOADED_BASE = "static/uploaded/tmp.mp4"
+
+ds = DeepSpeech(model_path=STT_MODEL_PATH,
+                scorer_path=SCORER_MODEL_PATH,
+                result_json_path=result_json_path,
+                )
 
 
 @app.route('/', methods=["GET", "POST"])
@@ -14,6 +23,7 @@ def index():
 def upload_file():
     if request.method == "POST":
         f = request.files['file']
+        ds.set_file(UPLOADED_BASE)
         f.save(UPLOADED_BASE)
         return render_template('dashboard.html')
 
@@ -25,7 +35,10 @@ def process():
         if status == "top":
             return render_template("index.html")
         else:
-            return render_template("dashboard.html")
+            ds.take_audio_info()
+            ds.take_audio()
+            sentence = ds.speech2text()
+            return render_template("dashboard.html", sentence=sentence)
 
 
 if __name__ == '__main__':
