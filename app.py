cc1286943a68290ed120b26656c49e91a5740a55
Bob <kohei.suzuki808@gmail.com>
2020-08-02 14:10:22 +0900

    Added a function to make possible to download a zip file that contains a json and a text file of transcripts

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 from lib.DeepSpeech import DeepSpeech
+import os
+import zipfile
 
 app = Flask(__name__, static_folder='static')
 
 STT_MODEL_PATH = "model/deepspeech-0.7.4-models.pbmm"
 SCORER_MODEL_PATH = "model/deepspeech-0.7.4-models.scorer"
 result_json_path = "results/result.json"
+result_txt_path = "results/result.txt"
 UPLOADED_BASE = "static/uploaded/tmp.mp4"
 
 ds = DeepSpeech(model_path=STT_MODEL_PATH,
                 scorer_path=SCORER_MODEL_PATH,
                 result_json_path=result_json_path,
+                result_txt_path=result_txt_path
                 )
 
 
@@ -38,7 +42,26 @@ def process():
             ds.take_audio_info()
             ds.take_audio()
             sentence = ds.speech2text()
-            return render_template("dashboard.html", sentence=sentence)
+            result_status = True
+            return render_template("dashboard.html",
+                                   sentence=sentence,
+                                   result_status=result_status)
+
+
+@app.route('/download/', methods=['GET', 'POST'])
+def download():
+    print("download")
+    zipf = zipfile.ZipFile('transcripts.zip', 'w', zipfile.ZIP_DEFLATED)
+    zipdir("results/", zipf)
+    zipf.close()
+    return send_file("transcripts.zip", as_attachment=True)
+
+
+def zipdir(path, ziph):
+    # ziph is zipfile handle
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            ziph.write(os.path.join(root, file))
 
 
 if __name__ == '__main__':
